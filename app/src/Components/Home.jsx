486d9e1324184fc6c4329885a1038b7ffaabf265
Frank Okezie Obiedere <obiedereokezie@gmail.com>
2020-08-01 22:10:39 +0100

    Updated Home page

--- app/src/Components/Home.jsx
+++ app/src/Components/Home.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
+import {
+  AppBar, Toolbar, Typography, Button,
+} from '@material-ui/core';
+import { Money } from '@material-ui/icons';
 import Mono from '../Mono';
 
 const useStyles = makeStyles((theme) => ({
@@ -27,11 +25,9 @@ export default function ButtonAppBar() {
     <div className={classes.root}>
       <AppBar position="static">
         <Toolbar>
-          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-            <MenuIcon />
-          </IconButton>
+          <Money className={classes.menuButton} />
           <Typography variant="h6" className={classes.title}>
-            News
+            Cool Fintech App
           </Typography>
           <Button color="inherit" onClick={() => { Mono().open(); }}>Login</Button>
         </Toolbar>
